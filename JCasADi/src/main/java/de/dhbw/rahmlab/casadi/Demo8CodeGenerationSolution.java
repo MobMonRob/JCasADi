3916ba8f896cdc4f938e6ba781bdf6f09412ebeb
Oliver Rettig <Oliver.Rettig@orat.de>
2023-11-27 12:04:12 +0100

    fix seltsames Problem im Demo8 File

--- JCasADi/src/main/java/de/dhbw/rahmlab/casadi/Demo8CodeGenerationSolution.java
+++ JCasADi/src/main/java/de/dhbw/rahmlab/casadi/Demo8CodeGenerationSolution.java
@@ -1,6 +1,7 @@
 package de.dhbw.rahmlab.casadi;
 
 import de.dhbw.rahmlab.casadi.impl.casadi.Function;
+import de.dhbw.rahmlab.casadi.impl.casadi.GenericType;
 import de.dhbw.rahmlab.casadi.impl.casadi.SX;
 import de.dhbw.rahmlab.casadi.impl.std.Dict;
 import de.dhbw.rahmlab.casadi.impl.std.StdVectorSX;
@@ -10,20 +11,25 @@ import de.dhbw.rahmlab.casadi.impl.std.StdVectorSX;
  */
 public class Demo8CodeGenerationSolution {
     
-    // Ex 1.1
+    public static void main(String[] args) {
+        
     
-    SX x = SX.sym("x", 2);
-    SX y = SX.sym("y");
-    
-    Function f = new Function("f", new StdVectorSX(new SX[]{x,y}), 
-                                       new StdVectorSX(new SX[]{SX.minus(SX.sqrt(x),y)}));
-    f.generate("fun");
+        // Ex 1.1
 
+        SX x = SX.sym("x", 2);
+        SX y = SX.sym("y");
+
+        Function f = new Function("f", new StdVectorSX(new SX[]{x,y}), 
+                                           new StdVectorSX(new SX[]{SX.minus(SX.sqrt(x),y)}));
+        f.generate("fun");
+
+
+        // Ex 1.2
+
+        Dict options = new Dict();
+        options.put("with_header", new GenericType(true));
+
+        //f.generate("fun2",options);
     
-    // Ex 1.2
-    
-    Dict options = new Dict();
-    options.put("with_header", new GenericType(true));
-       
-    f.generate("fun2",options);
+    }
 }
