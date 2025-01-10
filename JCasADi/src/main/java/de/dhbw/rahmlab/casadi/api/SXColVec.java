1d08cd6c3956bf6ba113449b6f72736204fd17a8
Oliver Rettig <Oliver.Rettig@orat.de>
2025-01-10 10:21:39 +0100

    new SXColVec constructor needed in CGACasadi to implement normalize(), exp(), sqrt()

--- JCasADi/src/main/java/de/dhbw/rahmlab/casadi/api/SXColVec.java
+++ JCasADi/src/main/java/de/dhbw/rahmlab/casadi/api/SXColVec.java
@@ -1,6 +1,8 @@
 package de.dhbw.rahmlab.casadi.api;
 
 import de.dhbw.rahmlab.casadi.impl.casadi.SX;
+import de.dhbw.rahmlab.casadi.impl.casadi.SXElem;
+import de.dhbw.rahmlab.casadi.impl.std.StdVectorCasadiSXElem;
 
 /**
  * @author Oliver Rettig (Oliver.Rettig@orat.de)
@@ -12,5 +14,15 @@ public class SXColVec {
     public SXColVec(SX sx){
         if (sx.sparsity().columns() != 1) throw new IllegalArgumentException("sx is no column vector");
         this.sx = sx;
+    }
+    // not yet tested
+    public SXColVec(SX sx, int[] indizes){
+        SXElem[] colElements = new SXElem[indizes.length];
+        for (int i=0;i<indizes.length;i++){
+            colElements[i] = sx.at(indizes[i]).scalar();
+        }
+        StdVectorCasadiSXElem x = new 
+            StdVectorCasadiSXElem(colElements);
+        sx = new SX(x);
     }
 }
