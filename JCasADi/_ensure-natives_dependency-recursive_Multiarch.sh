808e0a176a897a169510b2bfd07b453ecb00368a
codedeedope <26957536+codedeedope@users.noreply.github.com>
2025-04-23 14:14:55 +0200

    Fix ensure script

--- JCasADi/_ensure-natives_dependency-recursive_Multiarch.sh
+++ JCasADi/_ensure-natives_dependency-recursive_Multiarch.sh
@@ -8,8 +8,10 @@ source "./_bash_config.sh"
 
 perPlatform() {
 	if [[ "$(isSuccessTokenSet)" == "false" ]]; then
-		./_regenerate-natives_dependency-recursive_Multiarch.sh
+		bash "$wrapperDir/_ensure_dependency-recursive_Multiarch.sh"
 	fi
+
+	./_regenerate-natives_local_Multiarch.sh
 }
 
 run() {
