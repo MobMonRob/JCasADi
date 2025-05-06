042d92d6324efac96ccc1bd836b63423f66d5cbf
codedeedope <26957536+codedeedope@users.noreply.github.com>
2025-05-06 16:34:01 +0200

    Partially fix Windows build

--- casadi_customBuild/build_Windows64.sh
+++ casadi_customBuild/build_Windows64.sh
@@ -19,11 +19,30 @@ run() {
 
 	export LD_RUN_PATH="\$ORIGIN"
 
-	# Zum Testen des Bauens nur (Schneller Build, Langsame Ausführung): -DCMAKE_CXX_FLAGS:STRING="-O0" -DCMAKE_BUILD_TYPE="Debug"
-	# Für Release: -DCMAKE_BUILD_TYPE="Release"
-	FC=x86_64-w64-mingw32-gfortran-posix cmake -DCMAKE_BUILD_TYPE="Release" -DCMAKE_TOOLCHAIN_FILE="$scriptDir/mingw.cmake" "$scriptDir/$casadiDir" -DWITH_EXTRA_CHECKS=OFF -DWITH_SO_VERSION=OFF -DWITH_SELFCONTAINED=ON -DWITH_BUILD_REQUIRED=ON -DWITH_EXAMPLES=OFF -DCMAKE_INSTALL_PREFIX="$localTarget2" -DWITH_BUILD_SUNDIALS=OFF -DWITH_SUNDIALS=OFF -DWITH_BUILD_CSPARSE=OFF -DWITH_CSPARSE=OFF -DWITH_BUILD_TINYXML=OFF -DWITH_TINYXML=OFF -DWITH_MOCKUP_REQUIRED=OFF -DWITH_IPOPT=ON -DWITH_BUILD_IPOPT=ON -DWITH_MUMPS=ON -DWITH_BUILD_MUMPS=ON -DWITH_METIS=ON -DWITH_BUILD_METIS=ON -DWITH_FATROP=ON -DWITH_BUILD_FATROP=ON -DWITH_LAPACK=ON -DWITH_BUILD_LAPACK=ON -DWITH_BLASFEO=ON -DWITH_BUILD_BLASFEO=ON
+	export FC=x86_64-w64-mingw32-gfortran-posix
+	export CC=x86_64-w64-mingw32-gcc-posix
+	export CXX=x86_64-w64-mingw32-g++-posix
+
+	# needed for -DCMAKE_CROSS_COMPILING=ON
+	export DLLTOOL=x86_64-w64-mingw32-dlltool
+
+	#Um rdynamic Fehler mit mingw bei den Plugins zu verhindern:
+	#Tut: CMAKE_ARGS: -DCMAKE_TOOLCHAIN_FILE=/home/fabian/Desktop/JCasADi/casadi_customBuild/mingw.cmake
+
+	#--debug-trycompile
+	#-DCMAKE_C_COMPILER_WORKS=1
+
+	# Zum Testen des Bauens nur (Schneller Build, Langsame Ausführung): -DCMAKE_BUILD_TYPE="Dummy"
+	### Dieser ist CMake nicht bekannt und wird von CasADi im Gegensatz zum leeren Type nicht überschrieben. Damit wird kein Optimierungsflag gesetzt beim GCC. Damit nimmt der GCC den Default, welcher -O0 ist.
+	### Funktioniert aber nicht für Plugins, wenn die ihre eigenen Optimierungen anders setzen.
+	# Release: -DCMAKE_BUILD_TYPE="Release"
+	cmake -DCMAKE_SYSTEM_NAME="Windows" -DCMAKE_BUILD_TYPE="Dummy" -DCMAKE_CROSS_COMPILING=ON -DCMAKE_TOOLCHAIN_FILE="$scriptDir/mingw.cmake" "$scriptDir/$casadiDir" -DWITH_EXTRA_CHECKS=OFF -DWITH_SO_VERSION=OFF -DWITH_SELFCONTAINED=ON -DWITH_BUILD_REQUIRED=ON -DWITH_EXAMPLES=OFF -DCMAKE_INSTALL_PREFIX="$localTarget2" -DWITH_BUILD_SUNDIALS=OFF -DWITH_SUNDIALS=OFF -DWITH_BUILD_CSPARSE=OFF -DWITH_CSPARSE=OFF -DWITH_BUILD_TINYXML=OFF -DWITH_TINYXML=OFF -DWITH_MOCKUP_REQUIRED=OFF -DWITH_BLASFEO=ON -DWITH_BUILD_BLASFEO=ON
+	# -DWITH_EXTRA_CHECKS=OFF -DWITH_SO_VERSION=OFF -DWITH_SELFCONTAINED=ON -DWITH_BUILD_REQUIRED=ON -DWITH_EXAMPLES=OFF -DCMAKE_INSTALL_PREFIX="$localTarget2" -DWITH_BUILD_SUNDIALS=OFF -DWITH_SUNDIALS=OFF -DWITH_BUILD_CSPARSE=OFF -DWITH_CSPARSE=OFF -DWITH_BUILD_TINYXML=OFF -DWITH_TINYXML=OFF -DWITH_MOCKUP_REQUIRED=OFF -DWITH_IPOPT=ON -DWITH_BUILD_IPOPT=ON -DWITH_MUMPS=ON -DWITH_BUILD_MUMPS=ON -DWITH_METIS=ON -DWITH_BUILD_METIS=ON -DWITH_FATROP=ON -DWITH_BUILD_FATROP=ON -DWITH_LAPACK=ON -DWITH_BUILD_LAPACK=ON -DWITH_BLASFEO=ON -DWITH_BUILD_BLASFEO=ON
 	#/home/fabian/Desktop/minimal_cmake_example/
 
+	# DBG (auch für Compiler Flags)
+	#-v
+	#time nice -n19 ionice -c2 -n7 cmake --build . -j1
 	time nice -n19 ionice -c2 -n7 cmake --build . -j"$(nproc)"
 
 	rm -rdf "$localTarget2/casadi"
