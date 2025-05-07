1531128a90ab8329089d0c3bbd648e59f12e0826
codedeedope <26957536+codedeedope@users.noreply.github.com>
2025-05-07 14:58:12 +0200

    Adjust Linux equivalent to Windows

--- casadi_customBuild/_regenerate_local_Multiarch.sh
+++ casadi_customBuild/_regenerate_local_Multiarch.sh
@@ -9,8 +9,12 @@ source "./_bash_config.sh"
 run() {
 	clearCurrentPlatform
 
-	# Suboptimal. New download for each platform. But fast enough.
 	bash "./download_Noarch.sh"
+	if [[ "$currentPlatform" == "$platformWindows" ]]; then
+		# https://stackoverflow.com/questions/42386491/git-apply-skips-patches/62967602#62967602
+		# --directory is relative to the directory containing the .git of the current bash working directory.
+		git apply ./windows_casadi.patch --directory="casadi_customBuild/$casadiDir" --unsafe-paths -v
+	fi
 
 	bash "./build_$currentPlatform.sh"
 
--- casadi_customBuild/download_Noarch.sh
+++ casadi_customBuild/download_Noarch.sh
@@ -11,10 +11,6 @@ run() {
 	mkdir -p "$casadiDir"
 
 	git clone https://github.com/casadi/casadi.git --branch="3.7.0" --depth=1 --recursive "$casadiDir"
-
-	# https://stackoverflow.com/questions/42386491/git-apply-skips-patches/62967602#62967602
-	# --directory is relative to the directory containing the .git of the current bash working directory.
-	git apply ./casadi.patch --directory="casadi_customBuild/$casadiDir" --unsafe-paths -v
 }
 
 run_bash run $@
