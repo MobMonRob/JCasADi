d7a15e72b42e0d42c9694d7919b7c12819d51be0
codedeedope <26957536+codedeedope@users.noreply.github.com>
2025-05-14 18:15:31 +0200

    Added missing dll

--- casadi_customBuild/copy-mingW-deps_Windows64.sh
+++ casadi_customBuild/copy-mingW-deps_Windows64.sh
@@ -12,6 +12,8 @@ run() {
 	cp -L -u "$mingwLibPath1/libwinpthread-1.dll" "$currentTarget/casadi"
 	cp -L -u "$mingwLibPath2/libgcc_s_seh-1.dll" "$currentTarget/casadi"
 	cp -L -u "$mingwLibPath2/libstdc++-6.dll" "$currentTarget/casadi"
+	cp -L -u "$mingwLibPath2/libgfortran-5.dll" "$currentTarget/casadi"
+	cp -L -u "$mingwLibPath2/libquadmath-0.dll" "$currentTarget/casadi"
 }
 
 run_bash run $@
