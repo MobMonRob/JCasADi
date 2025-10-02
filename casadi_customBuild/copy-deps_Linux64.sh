466d6cc0a678afc96b93a5c83bc9f99cfeb5aed6
codedeedope <26957536+codedeedope@users.noreply.github.com>
2025-10-02 11:04:57 +0200

    Disable linux system libs copying.

--- casadi_customBuild/copy-deps_Linux64.sh
+++ casadi_customBuild/copy-deps_Linux64.sh
@@ -9,11 +9,13 @@ source "./_bash_config.sh"
 run() {
 	setCurrentPlatform "$platformLinux"
 
-	cp -L -u "/lib/x86_64-linux-gnu/libstdc++.so.6" "$currentTarget/casadi"
-	cp -L -u "/lib/x86_64-linux-gnu/libm.so.6" "$currentTarget/casadi"
-	cp -L -u "/lib/x86_64-linux-gnu/libgcc_s.so.1" "$currentTarget/casadi"
-	cp -L -u "/lib/x86_64-linux-gnu/libc.so.6" "$currentTarget/casadi"
-	cp -L -u "/lib/x86_64-linux-gnu/libgfortran.so.5" "$currentTarget/casadi"
+	# Deactivated. Can lead to linking errors.
+	# Executing system needs to have these installed instead.
+	#cp -L -u "/lib/x86_64-linux-gnu/libstdc++.so.6" "$currentTarget/casadi"
+	#cp -L -u "/lib/x86_64-linux-gnu/libm.so.6" "$currentTarget/casadi"
+	#cp -L -u "/lib/x86_64-linux-gnu/libgcc_s.so.1" "$currentTarget/casadi"
+	#cp -L -u "/lib/x86_64-linux-gnu/libc.so.6" "$currentTarget/casadi"
+	#cp -L -u "/lib/x86_64-linux-gnu/libgfortran.so.5" "$currentTarget/casadi"
 }
 
 run_bash run $@
