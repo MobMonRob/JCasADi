1a92c7b58120d022666dbcdd213b190fff8aa579
codedeedope <26957536+codedeedope@users.noreply.github.com>
2026-04-16 17:38:41 +0200

    Created simple API and added auto validation

---
+++ JCasADi_Maxima/src/main/java/de/dhbw/rahmlab/casadimaxima/MaximaProcessor.java
@@ -0,0 +1,44 @@
+package de.dhbw.rahmlab.casadimaxima;
+
+import de.dhbw.rahmlab.casadi.SxStatic;
+import de.dhbw.rahmlab.casadi.impl.casadi.SX;
+import de.dhbw.rahmlab.casadimaxima.casaditomaxima.MaximaTranspilerService;
+import de.dhbw.rahmlab.casadimaxima.maximatocasadi.CasadiTranspilerService;
+import java.util.List;
+
+public class MaximaProcessor {
+
+    public static synchronized SX simplify(SX expr, List<SX> variables) {
+        System.out.flush();
+        System.out.println();
+
+        SX casadiIn = SxStatic.sparsify(SxStatic.simplify(expr)); // To compare, what Maxima does better.
+        System.out.println("->casadiIn: " + casadiIn.toString());
+
+        String maximaIn = new MaximaTranspilerService().casadiToMaxima(casadiIn);
+        System.out.println("->maximaIn: " + maximaIn);
+
+        String maximaOut = new MaximaSimplifier().simplify(maximaIn);
+        System.out.println("->maximaOut: " + maximaOut);
+
+        SX casadiOut = new CasadiTranspilerService().maximaToCasadi(maximaOut, variables);
+        System.out.println("->casadiOut: " + casadiOut.toString());
+
+        // ---------------------
+        SX validatorIn = SxStatic.minus(casadiIn, casadiOut);
+        String maximaValIn = new MaximaTranspilerService().casadiToMaxima(validatorIn);
+        String maximaValOut = new MaximaSimplifier().simplify(maximaValIn);
+        SX casadiValOut = new CasadiTranspilerService().maximaToCasadi(maximaValOut, variables);
+        System.out.println("->casadiValOut: " + casadiValOut);
+        System.out.println();
+        System.out.flush();
+
+        if (casadiValOut.nnz_() != 0) {
+            throw new RuntimeException(String.format("Nonzeros in validation: %s", casadiValOut));
+        }
+        // ---------------------
+
+
+        return casadiOut;
+    }
+}
