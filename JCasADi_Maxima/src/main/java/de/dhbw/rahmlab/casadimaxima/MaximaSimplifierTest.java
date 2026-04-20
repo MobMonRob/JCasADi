a86ee8c897f99a83f36e39fdc7089a5c8c8e1b10
codedeedope <26957536+codedeedope@users.noreply.github.com>
2026-04-20 11:11:28 +0200

    Refactoring

---
+++ JCasADi_Maxima/src/main/java/de/dhbw/rahmlab/casadimaxima/MaximaSimplifierTest.java
@@ -0,0 +1,59 @@
+package de.dhbw.rahmlab.casadimaxima;
+
+import de.dhbw.rahmlab.casadi.DmStatic;
+import de.dhbw.rahmlab.casadi.SxStatic;
+import de.dhbw.rahmlab.casadi.impl.casadi.DM;
+import de.dhbw.rahmlab.casadi.impl.casadi.Function;
+import de.dhbw.rahmlab.casadi.impl.casadi.GenericType;
+import de.dhbw.rahmlab.casadi.impl.casadi.GlobalOptions;
+import de.dhbw.rahmlab.casadi.impl.casadi.MX;
+import de.dhbw.rahmlab.casadi.impl.casadi.SX;
+import de.dhbw.rahmlab.casadi.impl.std.Dict;
+import de.dhbw.rahmlab.casadi.impl.std.StdVectorSX;
+import de.dhbw.rahmlab.casadimaxima.api.MaximaSimplifier;
+import de.dhbw.rahmlab.casadimaxima.casaditomaxima.ToMaximaTranspilerService;
+import de.dhbw.rahmlab.casadimaxima.maximatocasadi.ToCasadiTranspilerService;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class MaximaSimplifierTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        String casadiString = "@1=-0.0996, @2=(arg0_0+(@1*arg1_0)), @3=0.0996, @4=((@3*arg1_0)-arg0_0), @5=0.5, @6=sq((arg0_0+(@1*arg1_0))), @7=((@5*@6)+-0.508884), @8=-0.5, @9=(-0.491116+(@8*@6)), @10=((@5*@6)+0.491116), @11=(0.508884+(@8*@6)), @12=((@3*arg1_0)-arg0_0), [(((@2*@4)-(@7*@9))+(@10*@11)), 00, 00, 00, (((@2*@9)-(@7*@4))-(@10*@12)), (((@2*@11)-(@7*@12))-(@10*@4)), 00, 00, 00, 00, 00, 00, 00, 00, 00, (((@2*@12)-(@7*@11))+(@10*@9)), 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00]";
+
+        // String casadiString = "@1=0.5, @2=sq(arg0_0), [arg0_0, 00, 00, 00, ((@1*@2)+-0.5), ((@1*@2)+@1), 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00]";
+        String maximaInput = new ToMaximaTranspilerService().casadiToMaxima(casadiString);
+        String maximaOutput = MaximaSimplifier.simplify(maximaInput);
+        SX sx = new ToCasadiTranspilerService().maximaToCasadi(maximaOutput, List.of(SxStatic.sym("arg0", 32, 1), SxStatic.sym("arg1", 32, 1)));
+        System.out.println("casadiIn: " + casadiString);
+        System.out.println("\nmaximaIn:\n" + maximaInput);
+        System.out.println("\nmaximaOut:\n" + maximaOutput);
+        System.out.println("\ncasadiOut:\n" + sx.toString());
+        var func = new Function("testfunc", new StdVectorSX(), new StdVectorSX(new SX[]{sx}), new Dict(Map.of("allow_free", new GenericType(true))));
+        func.free_sx().forEach(System.out::println);
+
+        SX a = new SX(1);
+        SX b = SxStatic.sym("b", 1, 1);
+        SX c = SxStatic.rdivide(a, b);
+        System.out.println("\nc:\n" + c.toString());
+
+        GlobalOptions.setSimplificationOnTheFly(false);
+
+        // SxStatic.set_scientific(true);
+        DmStatic.set_precision(10);
+        System.out.println(new DM(3.141592653589793239).toString());
+
+        SxStatic.set_precision(10);
+        SxStatic.set_width(10);
+        SxStatic.set_scientific(true);
+        System.out.println(new SX(3.141592653589793239));
+
+        System.out.println(new MX(3.141592653589793239));
+
+        // System.out.println(SxStatic.exp(new SX(1)).toString());
+    }
+}
--- JCasADi_Maxima/src/main/java/de/dhbw/rahmlab/casadimaxima/api/MaximaSimplifier.java
+++ JCasADi_Maxima/src/main/java/de/dhbw/rahmlab/casadimaxima/api/MaximaSimplifier.java
@@ -1,62 +1,45 @@
 package de.dhbw.rahmlab.casadimaxima.api;
 
-import de.dhbw.rahmlab.casadi.DmStatic;
 import de.dhbw.rahmlab.casadi.SxStatic;
-import de.dhbw.rahmlab.casadi.impl.casadi.DM;
-import de.dhbw.rahmlab.casadi.impl.casadi.Function;
-import de.dhbw.rahmlab.casadi.impl.casadi.GenericType;
-import de.dhbw.rahmlab.casadi.impl.casadi.GlobalOptions;
-import de.dhbw.rahmlab.casadi.impl.casadi.MX;
 import de.dhbw.rahmlab.casadi.impl.casadi.SX;
-import de.dhbw.rahmlab.casadi.impl.std.Dict;
-import de.dhbw.rahmlab.casadi.impl.std.StdVectorSX;
 import de.dhbw.rahmlab.casadimaxima.casaditomaxima.ToMaximaTranspilerService;
 import de.dhbw.rahmlab.casadimaxima.maximatocasadi.ToCasadiTranspilerService;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class MaximaSimplifier {
 
-    public static void main(String[] args) throws InterruptedException {
-        String casadiString = "@1=-0.0996, @2=(arg0_0+(@1*arg1_0)), @3=0.0996, @4=((@3*arg1_0)-arg0_0), @5=0.5, @6=sq((arg0_0+(@1*arg1_0))), @7=((@5*@6)+-0.508884), @8=-0.5, @9=(-0.491116+(@8*@6)), @10=((@5*@6)+0.491116), @11=(0.508884+(@8*@6)), @12=((@3*arg1_0)-arg0_0), [(((@2*@4)-(@7*@9))+(@10*@11)), 00, 00, 00, (((@2*@9)-(@7*@4))-(@10*@12)), (((@2*@11)-(@7*@12))-(@10*@4)), 00, 00, 00, 00, 00, 00, 00, 00, 00, (((@2*@12)-(@7*@11))+(@10*@9)), 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00]";
+    public static synchronized SX simplify(SX expr, List<SX> variables) {
+        System.out.flush();
+        System.out.println();
 
-        // String casadiString = "@1=0.5, @2=sq(arg0_0), [arg0_0, 00, 00, 00, ((@1*@2)+-0.5), ((@1*@2)+@1), 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00]";
-        String maximaInput = new ToMaximaTranspilerService().casadiToMaxima(casadiString);
-        String maximaOutput = new MaximaSimplifier().simplify(maximaInput);
-        SX sx = new ToCasadiTranspilerService().maximaToCasadi(maximaOutput, List.of(SxStatic.sym("arg0", 32, 1), SxStatic.sym("arg1", 32, 1)));
-        System.out.println("casadiIn: " + casadiString);
-        System.out.println("\nmaximaIn:\n" + maximaInput);
-        System.out.println("\nmaximaOut:\n" + maximaOutput);
-        System.out.println("\ncasadiOut:\n" + sx.toString());
-        var func = new Function("testfunc", new StdVectorSX(), new StdVectorSX(new SX[]{sx}), new Dict(Map.of("allow_free", new GenericType(true))));
-        func.free_sx().forEach(System.out::println);
+        SX casadiIn = SxStatic.sparsify(SxStatic.simplify(expr)); // To compare, what Maxima does better.
+        System.out.println("->casadiIn: " + casadiIn.toString());
 
-        SX a = new SX(1);
-        SX b = SxStatic.sym("b", 1, 1);
-        SX c = SxStatic.rdivide(a, b);
-        System.out.println("\nc:\n" + c.toString());
+        String maximaIn = new ToMaximaTranspilerService().casadiToMaxima(casadiIn);
+        System.out.println("->maximaIn: " + maximaIn);
 
-        GlobalOptions.setSimplificationOnTheFly(false);
+        String maximaOut = MaximaSimplifier.simplify(maximaIn);
+        System.out.println("->maximaOut: " + maximaOut);
 
-        // SxStatic.set_scientific(true);
-        DmStatic.set_precision(10);
-        System.out.println(new DM(3.141592653589793239).toString());
+        SX casadiOut = new ToCasadiTranspilerService().maximaToCasadi(maximaOut, variables);
+        System.out.println("->casadiOut: " + casadiOut.toString());
 
-        SxStatic.set_precision(10);
-        SxStatic.set_width(10);
-        SxStatic.set_scientific(true);
-        System.out.println(new SX(3.141592653589793239));
+        validate(casadiIn, casadiOut, variables);
 
-        System.out.println(new MX(3.141592653589793239));
+        System.out.println();
+        System.out.flush();
 
-        // System.out.println(SxStatic.exp(new SX(1)).toString());
+        return casadiOut;
     }
 
-    public String simplify(String expr) throws RuntimeException {
+    /**
+     * Only for debugging.
+     */
+    @Deprecated
+    public static String simplify(String expr) throws RuntimeException {
         try {
             String maximaInput = "";
             maximaInput += "display2d:false$\n"; // Do not visualize formulas
@@ -90,6 +73,20 @@ public class MaximaSimplifier {
             return out;
         } catch (Exception e) {
             throw new RuntimeException("Failed to run Maxima", e);
+        }
+    }
+
+    public static void validate(SX casadiIn, SX casadiOut, List<SX> variables) throws RuntimeException {
+        SX validatorIn = SxStatic.minus(casadiIn, casadiOut);
+        String maximaValIn = new ToMaximaTranspilerService().casadiToMaxima(validatorIn);
+        String maximaValOut = MaximaSimplifier.simplify(maximaValIn);
+        SX casadiValOut = new ToCasadiTranspilerService().maximaToCasadi(maximaValOut, variables);
+        System.out.println("->casadiValOut: " + casadiValOut);
+
+        if (casadiValOut.nnz_() != 0) {
+            System.out.println();
+            System.out.flush();
+            throw new RuntimeException(String.format("Nonzeros in validation: %s", casadiValOut));
         }
     }
 }
